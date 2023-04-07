02b4fa567293a0d7b9282bfa663d160dd984cba3
90192 <54006029+90192@users.noreply.github.com>
2023-04-07 18:36:37 -0400

    Update nypass.py

--- nypass.py
+++ nypass.py
@@ -1,11 +1,15 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup as bs
 
 
 def get_content(url):
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
-    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(options=options,
+                              service=Service(ChromeDriverManager().install())
+                              )
     driver.get(url)
     content = driver.execute_script('return document.body.innerHTML;')
     soup = bs(content, features='html.parser')
