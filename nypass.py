d7e3bda490a17bc774123aba591638ba0ee62476
90192 <54006029+90192@users.noreply.github.com>
2023-04-07 18:54:09 -0400

    Update nypass.py

--- nypass.py
+++ nypass.py
@@ -2,14 +2,18 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup as bs
+import tempfile
 
 
 def get_content(url):
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
-    driver = webdriver.Chrome(options=options,
-                              service=Service(ChromeDriverManager().install())
-                              )
+    with tempfile.TemporaryDirectory() as tmp:
+        driver_path = ChromeDriverManager(cache_path=tmp).install()
+        driver = webdriver.Chrome(driver_path, options=options)
+    # driver = webdriver.Chrome(options=options,
+    #                           service=Service(ChromeDriverManager().install())
+    #                           )
     driver.get(url)
     content = driver.execute_script('return document.body.innerHTML;')
     soup = bs(content, features='html.parser')
