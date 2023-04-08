b83420522d42aa7e02151ada734b527feec2dd38
90192 <54006029+90192@users.noreply.github.com>
2023-04-07 20:24:12 -0400

    Update nypass.py

--- nypass.py
+++ nypass.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from bs4 import BeautifulSoup as bs
 
 
@@ -6,8 +7,11 @@ def get_content(url):
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
-    driver = webdriver.Chrome(executable_path='driver/chromedriver', options=options)
+    service = Service('driver/chromedriver')
+    service.start()
+    driver = webdriver.Remote(service.service_url)
     driver.get(url)
     content = driver.execute_script('return document.body.innerHTML;')
+    driver.quit()
     soup = bs(content, features='html.parser')
     return soup.get_text()
